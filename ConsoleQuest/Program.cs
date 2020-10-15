1f56c2c12a9e1d46889dd61e1432ba0b4216cf48
PHPpriest <44893659+PHPpriest@users.noreply.github.com>
2020-10-15 08:47:55 +0500

    Добавил классы персонажей

--- ConsoleQuest/Program.cs
+++ ConsoleQuest/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleQuest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Enemy Enemy = new Enemy();
+            Player Player = new Player();
         }
+    }
+    class Enemy
+    {
+
+        public void Talk(string text, int ms_delay)
+        {
+
+        }
+    }
+
+    class Player
+    {
+        public void Talk(string text, int ms_delay)
+        {
+
+        }
+
     }
 }
